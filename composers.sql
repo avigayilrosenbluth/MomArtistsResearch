449928ac1523c26842b673bf35d5f572782cb3fb
avigayilrosenbluth <113922507+avigayilrosenbluth@users.noreply.github.com>
2022-09-28 10:09:31 -0400

    Update composers.sql

--- composers.sql
+++ composers.sql
@@ -12,3 +12,27 @@ CREATE TABLE COMPOSERS (
 INSERT INTO COMPOSERS (NAME)
 SELECT DISTINCT COMPOSER
 FROM SONGS;
+
+#To find the number of songs being performed by each composer, I'll use the count function and insert that integer.
+
+SELECT COUNT(SONG_ID) FROM SONGS WHERE COMPOSER = 'Aaron Copland';
+UPDATE COMPOSERS 
+SET SONGS = 1, ERA = 'Modern', NATIONALITY = 'American'
+WHERE NAME = 'Aaron Copland';
+
+SELECT COUNT(SONG_ID) FROM SONGS WHERE COMPOSER = 'Aaron Jay Kernis';
+UPDATE COMPOSERS 
+SET SONGS = 1, ERA = 'Modern', NATIONALITY = 'American'
+WHERE NAME = 'Aaron Jay Kernis';
+
+SELECT COUNT(SONG_ID) FROM SONGS WHERE COMPOSER = 'Alberto Ginastera';
+UPDATE COMPOSERS 
+SET SONGS = 1, ERA = 'Modern', NATIONALITY = 'Argentinian'
+WHERE NAME = 'Alberto Ginastera';
+
+SELECT COUNT(SONG_ID) FROM SONGS WHERE COMPOSER = 'Alexandre Desplat';
+UPDATE COMPOSERS 
+SET SONGS = 1, ERA = 'Modern', NATIONALITY = 'French'
+WHERE NAME = 'Alexandre Desplat';
+
+SELECT COUNT(SONG_ID) FROM SONGS WHERE COMPOSER = 'Anton Bruckner';
